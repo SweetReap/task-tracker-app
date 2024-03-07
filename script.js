106731fb7ad0df311ab44e30ce23dcd777b99322
Samantha Reape <54642592+SweetReap@users.noreply.github.com>
2024-03-07 15:12:59 -0500

    Implement task Rendering

--- script.js
+++ script.js
@@ -30,7 +30,18 @@ function handleSubmission(e) {
 }
 
 function render() {
-
+  //for every task in the tasks array, create a new task in the table
+  taskTable.innerHTML = tasks.map(task =>
+    `
+      <tr>
+        <td>${task.name}</td>
+        <td>${task.description}</td>
+        <td>${task.deadline}</td>
+        <td><button onclick="markTaskComplete(this)">Complete</button></td>
+        <td><button onclick="removeTask(this)">Remove</button></td>
+      </tr>
+    `
+  ).join('');
 }
 
 function init() {
